47177a4713e6ec0dc46b8f7b5f50adcabca5851b
nitz-prnv <nitprnv@gmail.com>
2021-10-25 20:42:08 +0530

    style works

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 import React from "react";
+import './app.css'
 export default function App() {
   return (
-    <>
-      <h3>Yaayy You have made it !</h3>
-    </>
+   <div className="App">
+     <div className="App-Header">
+      YAYY!! You Made It !
+     </div> 
+     <img src="https://c.tenor.com/ePTTWhc092kAAAAC/good-job.gif" alt="*claps*"/>
+     Go ahead and start editing 
+     <code>src/app.js</code>
+   </div>
   );
 }
